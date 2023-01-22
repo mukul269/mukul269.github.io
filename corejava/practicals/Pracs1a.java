8f7df3bc7bcfbd877af722b2e16dd5ebe481f82e
mukul269 <mkd44824744@gmail.com>
2023-01-22 14:27:13 +0530

    target blank

--- corejava/practicals/Pracs1a.java
+++ corejava/practicals/Pracs1a.java
@@ -21,10 +21,64 @@ class Pracs1a
 }//class Pracs1a
 
 
+//Practical 1-b
+/*Write a Java program to display the following pattern
+	Reversed Pyramid Star Pattern*/
+
+	class Pracs1b
+	{
+			public static void main(String[] args)
+		{
+				int i, j, k, n=5;
+		 
+				for (i= 0; i<=n-1; i++)
+				{
+						for (j=0; j<=i; j++) 
+						{	
+								System.out.print(" ");  //Space
+						}//j
+						for (k=0; k<=n-1-i; k++)
+						{
+								System.out.print("*" + " ");
+						}//k
+						System.out.println(); //Leave a line
+				}//i     
+		}//main()
+	}//class Prac
 
 
+	
 
+//Practical 1-c
+//Write a Java program to print the area and perimeter of a circle.
 
+import java.util.Scanner; //Package
+import java.text.DecimalFormat; // text - Package, DecimalFormat - class
+import java.math.RoundingMode;
+
+class Pracs1c
+{
+	//The java.text.DecimalFormat class is used to format numbers using a formatting pattern you specify yourself
+	private static DecimalFormat df2 = new DecimalFormat("#.##");
+
+	public static void main(String [] args)
+   	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the radius of circle : ");
+		double radius = sc.nextDouble();
+		   
+		System.out.println("\n--------Area of a Circle--------");   		
+      
+      	double area = Math.PI * (radius * radius);
+      	System.out.println("Area of Circle is " + df2.format(area));
+
+		System.out.println("\n--------Perimeter of a Circle--------");
+      
+      	double perimeter= 2 * Math.PI *radius;
+      	System.out.println("Perimeter of the circle is "+ df2.format(perimeter));
+   	}//main()
+}//class Pracs1c
 
 
 
--- corejava/practicals/Pracs3a.java
+++ corejava/practicals/Pracs3a.java
@@ -46,3 +46,72 @@ class Pracs3a
 		System.out.println("Other characters = " + others);
 	}//main()
 }//class Pracs3a
+
+
+
+//Practical 3-b
+/*
+Implement a Java function that calculates the sum of digits for a given char array consisting of the digits '0' to '9'. The function should return the digit sum as a long value.
+*/
+
+import java.util.*;
+class Pracs3b
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		String s=sc.nextLine();
+		SumOfDigits(s);
+	}//main()
+
+	public static void SumOfDigits(String str)
+	{
+		int sum=0, d=0;
+
+/*toCharArray() method converts the given string into a sequence of characters. 
+The returned array length is equal to the length of the string
+*/
+		char ch[]=str.toCharArray();
+		
+		for(int i=0;i<str.length();i++)
+		{
+			if(Character.isDigit(ch[i]))
+			{
+/*java.lang.Character.getNumericValue(char ch) returns the int value that the specified Unicode character represents*/
+				sum+=Character.getNumericValue(ch[i]);
+			}//if
+		}//i
+		
+		System.out.println("The sum of the digits is "+ sum);
+	}//SumOfDigits()
+}//class Pracs3b
+
+
+
+//Practical 3-c
+//Find the smallest and largest element from the array
+
+import java.util.*;
+class Pracs3c
+{
+	public static void main(String args[])
+	{
+		int large,small,i;
+		int a[] = new int[]{101, 2023, 333, 409, 5102};
+		int n = a.length;
+		large=small=a[0];
+
+		for(i=1;i<n;++i)
+		{
+			if(a[i]>large)
+				large=a[i];
+
+			if(a[i]<small)
+				small=a[i];
+		}//i
+	
+		System.out.print("Smallest element : " + small);
+		System.out.print("\n\nLargest element : " + large);
+	}//main()
+}//class Pracs3c
