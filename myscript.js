fd565f297117a48d84f03064d52a4468042d8d4b
mukul269 <mkd44824744@gmail.com>
2023-06-16 23:56:46 +0530

    added side nav and spm page

---
+++ myscript.js
@@ -0,0 +1,11 @@
+ function openNav()
+            {
+                document.getElementById("mySidenav").style.width = "412px";
+                //document.getElementById("table-wrapper").style.filter = "blur(8px)";                      
+            }
+          
+            function closeNav() 
+            {
+                document.getElementById("mySidenav").style.width = "0";
+                //document.getElementById("table-wrapper").style.filter = "blur(0px)";
+            }
