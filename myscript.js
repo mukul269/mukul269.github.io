89fd754c83e371cf9ff3b76056804a4f940ecaf3
mukul269 <mkd44824744@gmail.com>
2024-02-20 02:29:20 +0530

    .

--- myscript.js
+++ myscript.js
@@ -9,3 +9,38 @@
                 document.getElementById("mySidenav").style.width = "0";
                 //document.getElementById("table-wrapper").style.filter = "blur(0px)";
             }
+        
+            setTimeout(function() {
+                document.getElementById("splash-screen").style.transform = "translateY(-150vh)";
+            }, 2000); // Adjust timing as needed (in milliseconds)
+            
+            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAfkB08Z_C80b46nlyrzf8BvgfOqyNsv1g",
+    authDomain: "genyuva-6c7f3.firebaseapp.com",
+    databaseURL: "https://genyuva-6c7f3-default-rtdb.firebaseio.com",
+    projectId: "genyuva-6c7f3",
+    storageBucket: "genyuva-6c7f3.appspot.com",
+    messagingSenderId: "28526423082",
+    appId: "1:28526423082:web:4c88376539abbe1f56e753",
+    measurementId: "G-15D06LX77L"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  var genyuvaloginDB = firebase.database().ref('genyuvalogin');
+
+  document.getElementById('genyuvaloginform').addEventListener("submit", submitcredential)
+
+  function submitcredential(e){
+    e.preventDefault();
+
+    var name = getElementVal('uname');
+    var passsword = getElementVal('password');
+
+    console.log(name, password);
+  }
+
+  const getElementVal = (id) =>{
+    return document.getElementById(id).ariaValueMax;
+  };
